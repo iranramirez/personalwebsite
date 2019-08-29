90216c22922e35ac13c8aff02b3fc47ef9ad4e04
iranramirez <iran.ramirez@tamu.edu>
2019-08-29 16:46:11 -0500

    Project place cards set

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl'
 
 
 const Projects = () => {
 
     return (
-        <h1>Projects</h1>
+        <section>
+            <Grid>
+                <Cell col={12}>
+                    <div className="project-grid">
+                        {/* Project 1 */ }
+                        <Card shadow={5} style={{width: '512px', margin: '50px auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Get Started</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        {/* Project 2 */ }
+                        <Card shadow={5} style={{width: '512px', margin: '50px auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Project #2</CardTitle>
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Get Started</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        {/* Project 3 */ }
+                        <Card shadow={5} style={{width: '512px', margin: '50px auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Get Started</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        {/* Project 4 */ }
+                        <Card shadow={5} style={{width: '512px', margin: '50px auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                            <CardText>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Get Started</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
+                </Cell>
+            </Grid>
+        </section>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
