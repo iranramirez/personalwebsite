3dfa339d09e0f5615746f650d9992b7bd19e886f
iranramirez <iran.ramirez@tamu.edu>
2019-09-03 23:50:18 -0500

    Project Page done, Resume Page done, and Home Page done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl'
+import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl'
 
 
 const Projects = () => {
@@ -11,59 +11,118 @@ const Projects = () => {
                     <div className="project-grid">
                         {/* Project 1 */ }
                         <Card shadow={5} className="card-style">
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                            <CardTitle 
+                                style=
+                                    {{  color: 'black', 
+                                        height: '176px', 
+                                        background: 'url(https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/challenge_thumbnails/000/738/497/datas/original.png) center / cover'
+                                    }}
+                            >
+                                Alfred: Automated Flight Attendant
+                            </CardTitle>
                             <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                                Our team placed 4th overall and 2nd in the American Airlines challenge at TAMUhack.
+                                My expertise of electronics and software systems integration was used to 
+                                help our cart move and dispense a beverage once it arrived at it's destination.
+                                <br/>
+                                <br/>
+                                <br/>
                             </CardText>
                             <CardActions border>
-                                <Button colored>Get Started</Button>
+                                <Button colored>
+                                    <a 
+                                        href="https://devpost.com/software/robotic-flight-attendant-alfred" 
+                                        rel="noopener noreferrer" 
+                                        target="_blank"
+                                        style={{color: "inherit", textDecoration: "none"}}
+                                    >
+                                        Devpost
+                                    </a>
+                                </Button>
+                                <Button colored>
+                                    <a 
+                                        href="https://www.instagram.com/p/BtOXjKUnYvW/" 
+                                        rel="noopener noreferrer" 
+                                        target="_blank"
+                                        style={{color: "inherit", textDecoration: "none"}}
+                                    >
+                                        Live Demo
+                                    </a>
+                                </Button>
                             </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name="share" />
-                            </CardMenu>
                         </Card>
                         {/* Project 2 */ }
                         <Card shadow={5} className="card-style">
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Project #2</CardTitle>
+                        <CardTitle 
+                            style=
+                                {{  color: 'black',
+                                    height: '176px', 
+                                    background: 'url(https://yt3.ggpht.com/a/AGF-l7-sAisPdk0BZzT2zp0b9_Xl53Ys2UeWZ6o5qw=s900-c-k-c0xffffffff-no-rj-mo) center / cover'
+                                }}
+                        >
+                                Automated Residential Door Opener
+                        </CardTitle>
                             <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                                Our team placed 1st at Aggies Invent: Assistive Techonology 2018. We created a retrofitted system
+                                that allows a person with physical disabilites to be able to open their door unassisted. My expertise in electronics and software integration was used to
+                                help open the door remotely using ngrok, arduino, and DC motors.
                             </CardText>
                             <CardActions border>
-                                <Button colored>Get Started</Button>
+                                <Button colored>
+                                    <a 
+                                        href="https://www.instagram.com/p/BhXrfu3nBGu/" 
+                                        rel="noopener noreferrer" 
+                                        target="_blank"
+                                        style={{color: "inherit", textDecoration: "none"}}
+                                    >
+                                        Live Demo
+                                    </a>
+                                </Button>
                             </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name="share" />
-                            </CardMenu>
                         </Card>
                         {/* Project 3 */ }
                         <Card shadow={5} className="card-style">
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                            <CardTitle 
+                                className='card-title'
+                                style=
+                                    {{  color: 'white', 
+                                        height: '176px', 
+                                        background: 'url(https://thebite.aisb.ro/wp-content/uploads/personal-project.jpg) center / cover'
+                                    }}
+                            >
+                                Electornic Name Tag
+                            </CardTitle>
                             <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                                I made an electornic nametag for the Texas A&M Engineering 
+                                Career Fair. I used an Arduino, Arduino LCD Shield, 9V Battery, Barrel Jack adapter,
+                                and designed and 3D printed the housing and battery holder.
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
                             </CardText>
                             <CardActions border>
-                                <Button colored>Get Started</Button>
+                                <Button colored>
+                                    <a 
+                                        href="https://github.com/iranramirez" 
+                                        rel="noopener noreferrer" 
+                                        target="_blank"
+                                        style={{color: "inherit", textDecoration: "none"}}
+                                    >
+                                        Github
+                                    </a>
+                                </Button>
+                                <Button colored>
+                                    <a 
+                                        href="https://www.instagram.com/iranonoiran/" 
+                                        rel="noopener noreferrer" 
+                                        target="_blank"
+                                        style={{color: "inherit", textDecoration: "none"}}
+                                    >
+                                        Live Demo
+                                    </a>
+                                </Button>
                             </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name="share" />
-                            </CardMenu>
-                        </Card>
-                        {/* Project 4 */ }
-                        <Card shadow={5} className="card-style">
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
-                            <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris sagittis pellentesque lacus eleifend lacinia...
-                            </CardText>
-                            <CardActions border>
-                                <Button colored>Get Started</Button>
-                            </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name="share" />
-                            </CardMenu>
                         </Card>
                     </div>
                 </Cell>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,13 +2,205 @@ import React from 'react'
 import '../Styling.css'
 //import { Document, Page } from 'react-pdf';
 
-import resumePDF from './docs/IranRamirezResume.pdf';
+import resumePDF from './docs/IranResumeTech(Fall 2019).pdf';
 
 const Resume = () => {
 
     return(
       <div>
-        <embed className="pdf-viewport" width="60%" height="100%" name="plugin" id="plugin" src={resumePDF} type="application/pdf" internalinstanceid="4"></embed>
+        <embed className="pdf-viewport" height="80%" name="plugin" id="plugin" src={resumePDF} type="application/pdf" internalinstanceid="4"></embed>
+        <div className= 'relevant-courses'
+              style={{backgroundColor: 'white'}}>
+          <h3>Relavent Courses</h3>
+          <ul>
+            <li>
+              <a 
+                href="http://faculty.cse.tamu.edu/ritchey/courses/csce121/spring19/syllabus.pdf"
+                rel="noopener noreferrer"
+                target="_blank">
+                  CSCE 121
+              </a>
+              <br/>
+              Intro to Program Design and Concepts
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 219
+              </a>
+              <br/>
+              Digital Electronics
+            </li>
+            <br/>
+            <li> 
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 210
+              </a>
+              <br/>
+              Circuit Analysis
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 211
+              </a>
+              <br/>
+              Power Systems & Circuit Applications
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 350
+              </a>
+              <br/>
+              Analog Electronics
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 355
+              </a>
+              <br/>
+              EMAG & High Frequency Systems
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 455
+              </a>
+              <br/>
+              Wireless Transmission Systems
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 269
+              </a>
+              <br/>
+              Embedded Systems Development in C
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 349
+              </a>
+              <br/>
+              Microcontroller Architecture
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 369
+              </a>
+              <br/>
+              Embedded Systems Software
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 469
+              </a>
+              <br/>
+              Real Time Operating Systems (RTOS)
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 333
+              </a>
+              <br/>
+              Product Development
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 333
+              </a>
+              <br/>
+              Engineering Leadership
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 315
+              </a>
+              <br/>
+              Local Metropolitan Area Networks
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 415
+              </a>
+              <br/>
+              Advanced Network Security
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 352
+              </a>
+              <br/>
+              Electronic Test I
+            </li>
+            <br/>
+            <li>
+              <a 
+                href="https://catalog.tamu.edu/undergraduate/course-descriptions/eset/"
+                rel="noopener noreferrer"
+                target="_blank">
+                  ESET 329
+              </a>
+              <br/>
+              Six Sigma & Applied Statistics
+            </li>
+          </ul>
+        </div>
       </div>
           
     )
